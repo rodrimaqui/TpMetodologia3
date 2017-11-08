d47e245e5a222c15b0371bd1ad67e4ffc762dfe3
Lisandro Cooper <coopermegadeth@gmail.com>
2017-11-08 19:42:04 -0300

    wachin

--- saborto/saborto/urls.py
+++ saborto/saborto/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from Hospedajes.views import index
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', index)
 ]
+
