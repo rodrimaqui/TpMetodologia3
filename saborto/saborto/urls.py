81310bdea88a39fa3b703e39c0795c215e8e952b
rodri <rodri@rodri.rodri>
2017-11-10 16:35:19 -0300

    asd

--- saborto/saborto/urls.py
+++ saborto/saborto/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from Hospedajes.views import index
+from Hospedajes.views import *
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
