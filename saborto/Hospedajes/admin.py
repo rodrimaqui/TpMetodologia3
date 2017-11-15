e89916113cb0cac601c2600984562e3952923a38
rodri <rodri@rodri.rodri>
2017-11-15 15:19:27 -0300

    add DateRental inline with Property admin

--- saborto/Hospedajes/admin.py
+++ saborto/Hospedajes/admin.py
@@ -4,17 +4,21 @@ from Hospedajes.models import *
 class AdminCity(admin.ModelAdmin):
     search_fields = ['name']
 
+class DateRental_Inline(admin.TabularInline):
+    model = DateRental
+    fk_name = 'property'
+    max_num = 7
+
 class AdminPropery(admin.ModelAdmin):
     search_fields = ['numberCard']
     list_filter = ('numberCard',)
     ordering = ('title',)
+    inlines = [DateRental_Inline, ]
+
 
 # Register your models here.
 
 admin.site.register(City, AdminCity)
-admin.site.register(DateRental)
 admin.site.register(Guest)
 admin.site.register(Reservation)
-admin.site.register(Property,AdminPropery)
-
-
+admin.site.register(Property,AdminPropery,)
